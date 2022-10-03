9607b60bbc08e677fed0f4a5558cb0dd63f972eb
arch-havis <havisiqbalsyah@gmail.com>
2022-10-03 09:52:57 +0700

    feat(feature-pengambilan-rosok): adding handler

--- features/pengambilan_rosok/delivery/delivery.go
+++ features/pengambilan_rosok/delivery/delivery.go
@@ -2,7 +2,7 @@ package delivery
 
 import (
 	"net/http"
-	transaksiporter "rozhok/features/transaksi_porter"
+	pengambilanrosok "rozhok/features/pengambilan_rosok"
 	"rozhok/middlewares"
 	"rozhok/utils/helper"
 
@@ -10,34 +10,27 @@ import (
 )
 
 type TransaksiPorter struct {
-	Usecase transaksiporter.TransaksiPorterUsecase
+	Usecase pengambilanrosok.PengambilanRosokUsecase
 }
 
-func New(e *echo.Echo, usecase transaksiporter.TransaksiPorterUsecase) {
+func New(e *echo.Echo, usecase pengambilanrosok.PengambilanRosokUsecase) {
 	handler := &TransaksiPorter{
 		Usecase: usecase,
 	}
 
-	e.GET("/transaksi/porter", handler.GetAll, middlewares.JWTMiddleware(), middlewares.IsPorter)
-	e.GET("/transaksi/:transaction_id/porter", handler.Get, middlewares.JWTMiddleware(), middlewares.IsPorter)
-	e.PUT("/transaksi/:transaction_id/porter", handler.PutTransaksiPembelian, middlewares.JWTMiddleware(), middlewares.IsPorter)
-	e.POST("/transaksi/:transaction_id/porter", handler.PostTransaksiPenjualan, middlewares.JWTMiddleware(), middlewares.IsPorter)
+	e.GET("/pengambilan/porter", handler.Get)
+	e.GET("/pengambilan/:penjualan_id/porter", handler.GetAll)
+	e.POST("/pengambilan/:penjualan_id/porter", handler.PostTransaksiPenjualan)
 }
 
 func (deliv *TransaksiPorter) GetAll(c echo.Context) error {
-	startDate := c.QueryParam("start_date")
-	endDate := c.QueryParam("end_date")
-	tipeTransaksi := c.QueryParam("type_transaction")
 	idPorter, _, _ := middlewares.ExtractToken(c)
 
-	var TransaksiPorterCore transaksiporter.Core
+	var pengambilanBarangRosok pengambilanrosok.Core
 
-	TransaksiPorterCore.StartDate = startDate
-	TransaksiPorterCore.EndDate = endDate
-	TransaksiPorterCore.TipeTransaksi = tipeTransaksi
-	TransaksiPorterCore.PorterID = uint(idPorter)
+	pengambilanBarangRosok.PorterID = uint(idPorter)
 
-	transaksiList, err := deliv.Usecase.GetAll(TransaksiPorterCore)
+	transaksiList, err := deliv.Usecase.GetAll(pengambilanBarangRosok)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper(err.Error()))
 	}
@@ -52,35 +45,29 @@ func (deliv *TransaksiPorter) GetAll(c echo.Context) error {
 
 func (deliv *TransaksiPorter) Get(c echo.Context) error {
 	id := helper.ParamInt(c, "transaction_id")
-	tipeTransaksi := c.QueryParam("type_transaction")
 	idPorter, _, _ := middlewares.ExtractToken(c)
 
-	if tipeTransaksi == "" {
-		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper("query parameter type_transaction is required"))
-	}
+	var PengambilanRosokCore pengambilanrosok.Core
+	PengambilanRosokCore.ID = uint(id)
+	PengambilanRosokCore.PorterID = uint(idPorter)
 
-	var TransaksiPorterCore transaksiporter.Core
-	TransaksiPorterCore.ID = uint(id)
-	TransaksiPorterCore.TipeTransaksi = tipeTransaksi
-	TransaksiPorterCore.PorterID = uint(idPorter)
-
-	transaksiPorter, err := deliv.Usecase.Get(TransaksiPorterCore)
+	pengambilanRosokResult, err := deliv.Usecase.Get(PengambilanRosokCore)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, helper.SuccessDataResponseHelper("success get data", toResponse(transaksiPorter)))
+	return c.JSON(http.StatusOK, helper.SuccessDataResponseHelper("success get data", toResponse(pengambilanRosokResult)))
 }
 
 func (deliv *TransaksiPorter) PostTransaksiPenjualan(c echo.Context) error {
 	id := helper.ParamInt(c, "transaction_id")
 	idPorter, _, _ := middlewares.ExtractToken(c)
 
-	var TransaksiPorterCore transaksiporter.Core
-	TransaksiPorterCore.ID = uint(id)
-	TransaksiPorterCore.PorterID = uint(idPorter)
+	var PengambilanRosokCore pengambilanrosok.Core
+	PengambilanRosokCore.ID = uint(id)
+	PengambilanRosokCore.PorterID = uint(idPorter)
 
-	row, err := deliv.Usecase.PostTransaksiPenjualan(TransaksiPorterCore)
+	row, err := deliv.Usecase.CreatePengambilanRosok(PengambilanRosokCore)
 
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper(err.Error()))
@@ -91,39 +78,4 @@ func (deliv *TransaksiPorter) PostTransaksiPenjualan(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, helper.SuccessResponseHelper("success create data"))
-}
-
-func (deliv *TransaksiPorter) PutTransaksiPembelian(c echo.Context) error {
-	id := helper.ParamInt(c, "transaction_id")
-	var TransaksiRequestList []Request
-	var TransaksiPorterCore transaksiporter.Core
-
-	TransaksiPorterCore.ID = uint(id)
-
-	errBind := c.Bind(&TransaksiRequestList)
-	if errBind != nil {
-		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper(errBind.Error()))
-	}
-
-	// errValidate := c.Validate(&TransaksiRequestList)
-	// if errValidate != nil {
-	// 	return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper(errValidate.Error()))
-	// }
-
-	BarangRosokList := []transaksiporter.DetailTransaksiPorter{}
-	for _, barangRosok := range TransaksiRequestList {
-		BarangRosokList = append(BarangRosokList, toCore(barangRosok))
-	}
-	TransaksiPorterCore.DetailTransaksiPorter = BarangRosokList
-
-	row, err := deliv.Usecase.PutTransaksiPembelian(TransaksiPorterCore)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper(err.Error()))
-	}
-
-	if row < 1 {
-		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper("failed to update data"))
-	}
-
-	return c.JSON(http.StatusOK, helper.SuccessResponseHelper("success update data"))
 }
