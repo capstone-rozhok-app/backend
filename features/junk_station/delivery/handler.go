e0b033424ca2ca5f3f08838cbb4eea5ec553bad6
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-10-03 07:02:02 +0700

    add: crud junk station

--- features/junk_station/delivery/handler.go
+++ features/junk_station/delivery/handler.go
@@ -3,6 +3,8 @@ package delivery
 import (
 	js "rozhok/features/junk_station"
 	"rozhok/middlewares"
+	"rozhok/utils/helper"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,6 +25,54 @@ func (h *JunkHandler) CreateJunkStation(c echo.Context) error {
 	var JsRequest JsReq
 	errBind := c.Bind(&JsRequest)
 	if errBind != nil {
-		return c.JSON(400, helper)
+		return c.JSON(400, helper.FailedResponseHelper("failed to bind"))
 	}
+	row, err := h.JunkInterface.CreateJunkStation(ToCoreReq(JsRequest), jsID)
+	if err != nil || row != 1 {
+		return c.JSON(400, helper.FailedResponseHelper("failed to create"))
+	}
+	return c.JSON(200, helper.SuccessDataResponseHelper("Succses create Junk Station", jsID))
+}
+
+func (h *JunkHandler) GetJunkStationAll(c echo.Context)error {
+	res, err := h.JunkInterface.GetJunkStationAll()
+	if err != nil {
+		return c.JSON(400, helper.FailedResponseHelper("failed to get data"))
+	}
+	return c.JSON(200, helper.SuccessDataResponseHelper(("succses get data"), CoreList(res)))
+}
+
+func (h *JunkHandler) GetJunkStationById(c echo.Context)error {
+	idToken, _ ,_ := middlewares.ExtractToken(c)
+
+	idParam := c.Param("id")
+	idConv, errConv := strconv.Atoi(idParam)
+	if errConv != nil {
+		return c.JSON(400, helper.FailedResponseHelper("error get by param"))
+	}
+	result, err := h.JunkInterface.GetJunkStationById(idConv, idToken)
+	if err != nil {
+		return c.JSON(400, helper.FailedResponseHelper("error Get data"))
+	}
+	return c.JSON(200, helper.SuccessDataResponseHelper(("Succses get data"), FromCoreToResponse(result)))
+}
+
+func (h *JunkHandler) PutJunkStation(c echo.Context)error  {
+	idToken, _ ,_ := middlewares.ExtractToken(c)
+
+	idParam := c.Param("id")
+	idConv, errConv := strconv.Atoi(idParam)
+	if errConv != nil || idConv == 0 {
+		return c.JSON(400, helper.FailedResponseHelper("error update by param"))
+	}
+	var JunkRequest JsReq
+	errBind := c.Bind(&JunkRequest)
+	if errBind != nil {
+		return c.JSON(400, helper.FailedResponseHelper("error bind JS"))
+	}
+	row, err := h.JunkInterface.PutJunkStation(idConv, ToCoreReq(JunkRequest))
+	if err != nil || row == 0 {
+		return c.JSON(400, helper.FailedResponseHelper("error Update JS"))
+	}
+	return c.JSON(200, helper.SuccessDataResponseHelper("Succses update JS", idToken))
 }
--- features/junk_station/delivery/response.go
+++ features/junk_station/delivery/response.go
@@ -1,12 +1,53 @@
 package delivery
 
+import (
+	js "rozhok/features/junk_station"
+)
+
 type JSRes struct{
-	JunkStationID	int	`json:js_id`
-	JunkStationName	string	`json:js_name`
-	JunkStationOwner string	`json:js_owner`
-	Provinsi		string	`json:provinsi`
-	Kota			string `json:kota`
-	Kecamatan		string `json:kecamatan`
-	Telp			string	`json:telp`
-	Jalan			string	`json:jalan`
+	JunkStationID	int			`json:js_id`
+	JunkStationName	string		`json:js_name`
+	JunkStationOwner string		`json:js_owner`
+	Status			string 		`json:status`
+	Provinsi		string		`json:provinsi`
+	Kota			string 		`json:kota`
+	Kecamatan		string 		`json:kecamatan`
+	Telp			string		`json:telp`
+	Jalan			string		`json:jalan`
+}
+
+func FromCore(data js.Core) JSRes {
+	return JSRes{
+		JunkStationID: data.JunkStationID,
+		JunkStationName: data.JunkStationName,
+		JunkStationOwner: data.JunkStationOwner,
+		Status: data.Status,
+		Provinsi: data.Provinsi,
+		Kota: data.Kota,
+		Kecamatan: data.Kecamatan,
+		Telp: data.Telp,
+		Jalan: data.Jalan,
+	}
+}
+
+func CoreList(data []js.Core) []JSRes {
+	var res []JSRes
+	for _, v := range data {
+		res = append(res, FromCore(v))
+	}
+	return res
+}
+
+func FromCoreToResponse(data js.Core) JSRes {
+	dataResponse := JSRes{
+		JunkStationName: data.JunkStationName,
+		JunkStationOwner: data.JunkStationOwner,
+		Status: data.Status,
+		Provinsi: data.Provinsi,
+		Kota: data.Kota,
+		Kecamatan: data.Kecamatan,
+		Telp: data.Telp,
+		Jalan: data.Jalan,
+	}
+	return dataResponse
 }
