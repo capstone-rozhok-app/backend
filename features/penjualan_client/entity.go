31a3bf49e86158661c671b7d3cedd2d7db89578e
arch-havis <havisiqbalsyah@gmail.com>
2022-10-03 16:12:38 +0700

    feat(feature-penjualan-client): adding repo

---
+++ features/penjualan_client/entity.go
@@ -0,0 +1,22 @@
+package penjualanclient
+
+type Core struct {
+	ID           uint
+	KategoriID   uint
+	ClientID     uint
+	NamaKategori string
+}
+
+type PenjualanClientData interface {
+	GetAll(PenjualanClientCore Core) ([]Core, error)
+	Update(PenjualanClientCore Core) (int, error)
+	Insert(PenjualanClientCore Core) (int, error)
+	Delete(PenjualanClientCore Core) (int, error)
+}
+
+type PenjualanClientUsecase interface {
+	GetAll(PenjualanClientCore Core) ([]Core, error)
+	update(PenjualanClientCore Core) (int, error)
+	Store(PenjualanClientCore Core) (int, error)
+	Delete(PenjualanClientCore Core) (int, error)
+}
