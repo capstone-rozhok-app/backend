55165d59f60aab4ab12add73cfd2c6c629f44afe
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-10-04 19:43:14 +0700

    add: upload photo junk-station

--- features/junk_station/delivery/request.go
+++ features/junk_station/delivery/request.go
@@ -15,6 +15,7 @@ type JsReq struct {
 	Kecamatan			string	`json:"kecamatan" form:"kecamatan" validate:"required"`
 	Telp				string	`json:"no_telp" form:"telp" validate:"required"`
 	Jalan				string	`json:"jalan" form:"jalan" validate:"required"`
+	Image_url			string	`json:"image_url" form:"image_url" validate:"required"`
 }
 
 func FromCoreReq(req JsReq) js.Core{
@@ -28,6 +29,7 @@ func FromCoreReq(req JsReq) js.Core{
 		Kecamatan: 			req.Kecamatan,	
 		Telp: 				req.Telp,
 		Jalan:				req.Jalan,
+		Image_url: 			req.Image_url,
 	}
 }
 
@@ -42,6 +44,7 @@ func ToCoreReq(req JsReq) js.Core{
 		Kecamatan: 			req.Kecamatan,	
 		Telp: 				req.Telp,
 		Jalan:				req.Jalan,
+		Image_url: 			req.Image_url,	
 	}
 }
 
