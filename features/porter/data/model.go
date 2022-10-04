342b8cd71bcf7c26753a5491be49305bff0aff8a
arch-vizu <110443476+arch-havis@users.noreply.github.com>
2022-10-04 18:06:17 +0700

    HOTFIX (porter)

--- features/porter/data/model.go
+++ features/porter/data/model.go
@@ -13,7 +13,6 @@ type User struct {
 	Role            string
 	Username        string `gorm:"unique"`
 	StatusKemitraan string
-	Foto            string
 	Provinsi        string
 	Kota            string
 	Kecamatan       string
