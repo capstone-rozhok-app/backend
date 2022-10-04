1df116ff3ab7272bb823250812ea314605cdb3be
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-10-04 15:53:16 +0700

    add: kemitraan

--- features/kemitraan/delivery/request.go
+++ features/kemitraan/delivery/request.go
@@ -1,1 +1,19 @@
 package delivery
+
+import mitra "rozhok/features/kemitraan"
+
+type MitraRequest struct{
+	StatusKemitraan		string	`json:"status_kemitraan" form:"status_kemitraan" validate:"required"`
+}
+
+func FromCoreReq(r MitraRequest) mitra.MitraCore {
+	return mitra.MitraCore{
+		StatusKemitraan: r.StatusKemitraan,
+	}
+}
+
+func ToCore(r MitraRequest) mitra.MitraCore {
+	return mitra.MitraCore{
+		StatusKemitraan: r.StatusKemitraan,
+	}
+}
