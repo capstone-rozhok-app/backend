bb6b0d24500d59c406108f3d170f811a25745dc5
arch-havis <havisiqbalsyah@gmail.com>
2022-10-04 10:12:47 +0700

    feat(transaksi-client): add entity and model

--- features/produk/delivery/request.go
+++ features/produk/delivery/request.go
@@ -6,7 +6,7 @@ type ProdukRequest struct {
 	Nama      string `json:"nama_product" form:"nama_product" validate:"required"`
 	Image_url string `json:"image_url" form:"image_url"`
 	Stok      int    `json:"stok" form:"stok" validate:"required"`
-	Harga     int64  `json:"harga" form:"harga" validate:"required"`
+	Harga     int64 `json:"harga" form:"harga" validate:"required"`
 	Desc      string `json:"desc" form:"desc" validate:"required"`
 }
 
---
+++ features/transaksi_client/entity.go
@@ -0,0 +1,53 @@
+package transaksiclient
+
+type Core struct {
+	ID            uint
+	GrandTotal    int64
+	TipeTransaksi string
+	Status        string
+	StartDate     string
+	EndDate       string
+	KodeTransaksi string
+	Kurir         string
+
+	Client      User
+	Porter      User
+	BarangRosok []BarangRosok
+	Product     []Product
+}
+
+type User struct {
+	ID        uint
+	Name      string
+	NoTelp    string
+	Provinsi  string
+	Kota      string
+	Kecamatan string
+}
+
+type BarangRosok struct {
+	Kategori string
+	Berat    int64
+	Harga    int64
+}
+
+type Product struct {
+	ImageUrl    string
+	ProductName string
+	Qty         uint
+	Subtotal    int64
+}
+
+type TransaksiClientData interface {
+	GetAll(TransaksiClient Core) ([]Core, error)
+	Get(TransaksiClient Core) (Core, error)
+	Insert(TransaksiClient Core) (int, error)
+	Update(TransaksiClient Core) (int, error)
+}
+
+type TransaksiClientUsecase interface {
+	GetAll(TransaksiClient Core) ([]Core, error)
+	Get(TransaksiClient Core) (Core, error)
+	Create(TransaksiClient Core) (int, error)
+	Update(TransaksiClient Core) (int, error)
+}
