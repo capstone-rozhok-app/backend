fdf1280146261001656f680b6063fb80e2de4b20
arch-havis <havisiqbalsyah@gmail.com>
2022-10-05 18:12:05 +0700

    fix(client): add response username

--- features/client/delivery/response.go
+++ features/client/delivery/response.go
@@ -3,13 +3,15 @@ package delivery
 import "rozhok/features/client"
 
 type Response struct {
-	TotalJual int64 `json:"total_penjualan"`
-	Bonus     int64 `json:"bonus_terakhir"`
+	TotalJual int64  `json:"total_penjualan"`
+	Bonus     int64  `json:"bonus_terakhir"`
+	Username  string `json:"username"`
 }
 
 func fromCore(dataCore client.Core) Response {
 	return Response{
 		TotalJual: dataCore.TotalJual,
 		Bonus:     dataCore.Bonus,
+		Username:  dataCore.Username,
 	}
 }
