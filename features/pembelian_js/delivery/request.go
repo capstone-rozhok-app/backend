3ac9db0adbd87739cf099aa152b70c5d24e6bbc7
arch-havis <havisiqbalsyah@gmail.com>
2022-10-07 08:21:29 +0700

    fix(junk-station): fix junk station

--- features/pembelian_js/delivery/request.go
+++ features/pembelian_js/delivery/request.go
@@ -4,24 +4,24 @@ import (
 	pjs "rozhok/features/pembelian_js"
 )
 
-type PembelianRequest struct{
-	Kategori	int		`json:"kategori" form:"kategori" validate:"required"`
-	Berat		int64		`json:"berat" form:"berat" validate:"required"`
-	Harga		int64		`json:"harga" form:"harga" validate:"required"`
+type PembelianRequest struct {
+	Kategori int   `json:"id_kategori" form:"id_kategori" validate:"required"`
+	Berat    int64 `json:"berat" form:"berat" validate:"required"`
+	Harga    int64 `json:"harga" form:"harga" validate:"required"`
 }
 
 func FromCoreReq(r PembelianRequest) pjs.PembelianCore {
 	return pjs.PembelianCore{
 		Kategori: r.Kategori,
-		Berat: int(r.Berat),
-		Harga: int(r.Harga),
+		Berat:    int(r.Berat),
+		Harga:    int(r.Harga),
 	}
 }
 
 func ToCore(r PembelianRequest) pjs.PembelianCore {
 	return pjs.PembelianCore{
 		Kategori: r.Kategori,
-		Berat: int(r.Berat),
-		Harga: int(r.Harga),
+		Berat:    int(r.Berat),
+		Harga:    int(r.Harga),
 	}
 }
