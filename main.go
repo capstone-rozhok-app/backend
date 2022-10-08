8145acecf139237a835e05e741182b4369757e52
arch-havis <havisiqbalsyah@gmail.com>
2022-10-08 17:13:56 +0700

    fix cors

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, echo.HeaderWWWAuthenticate},
-		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
 	}))
 
 	e.Use(middleware.Recover())
